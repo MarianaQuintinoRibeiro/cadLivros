01824f59e86de5fbe2b8a8b19213302e70d0e5e1
MarianaQuintinoRibeiro <133990329+MarianaQuintinoRibeiro@users.noreply.github.com>
2023-11-09 10:31:10 -0300

    Delete src directory

--- src/main/java/com/MarianaQuintino/Gerenciamento/de/livros/repository/livroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.MarianaQuintino.Gerenciamento.de.livros.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.MarianaQuintino.Gerenciamento.de.livros.entities.livro;
-
-public interface livroRepository extends JpaRepository<livro, Long>{
-
-}
